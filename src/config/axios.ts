e50c7317ef21fd48098128d7df278046a9fe4399
pratchata <ratchata.pyc@gmail.com>
2024-07-27 15:47:45 +0700

    deploy

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8888";
+// axios.defaults.baseURL = "http://localhost:8888";
+axios.defaults.baseURL = "https://typescript-server-jm3s.onrender.com";
 
 export default axios;
